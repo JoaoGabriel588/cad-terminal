8ea90b3905fd436513d6b3f1601fbc0ef0fe93ae
João Gabriel <joao.silva588@aluno.ce.gov.br>
2020-10-14 10:43:46 -0300

    Iniciando o projeto App

---
+++ app.py
@@ -0,0 +1,15 @@
+import time
+
+cont = 0
+
+print("Programa baseado como funciona um cadastro em qualquer tipo de sistema/programa")
+
+print("Aguarde...")
+time.sleep(5)
+
+while cont != 6:
+
+	print("- - - - MENU DE CADASTRO - - - -")
+
+
+	input("")
